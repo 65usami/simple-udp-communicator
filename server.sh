2f4aafea2a6ad349b9c7f18ced28005f04c5ddd3
65usami <kenichi@usammi65.com>
2019-09-05 22:53:59 +0900

    bug fix shell variables

--- server.sh
+++ server.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-uname_m==`uname -m`
+uname_m=`uname -m`
 processor=${uname_m:0:3}
 platform="$OSTYPE"
 
-if [ processor = 'arm' ]; then
+if [ $processor = 'arm' ]; then
  # Raspberry Pi
  source venvs/venv_raspberry_pi/bin/activate
  python server.py $1
@@ -15,7 +15,7 @@ else
  }
  trap 'termination'  {1,2,3,15}
 
- if [[ platform == "darwin"* ]]; then
+ if [[ $platform == "darwin"* ]]; then
   # Mac
   source venvs/venv_mac/bin/activate
   python server.py $1
