8502a57fe7452974364b7fada3bee2a695edd2d9
65usami <kenichi@usammi65.com>
2019-09-05 22:43:51 +0900

    Add venvs for various platform

--- server.bat
+++ server.bat
@@ -1,5 +1,5 @@
 @echo off
 
-call venv_windows\Scripts\activate.bat
+call venvs\venv_windows\Scripts\activate.bat
 python server.py %1
-call venv_windows\Scripts\deactivate.bat
+call venvs\venv_windows\Scripts\deactivate.bat
--- server.sh
+++ server.sh
@@ -1,11 +1,30 @@
 #!/bin/sh
 
-function termination() {
+uname_m==`uname -m`
+processor=${uname_m:0:3}
+platform="$OSTYPE"
+
+if [ processor = 'arm' ]; then
+ # Raspberry Pi
+ source venvs/venv_raspberry_pi/bin/activate
+ python server.py $1
+ deactivate
+else
+ function termination() {
+   deactivate
+ }
+ trap 'termination'  {1,2,3,15}
+
+ if [[ platform == "darwin"* ]]; then
+  # Mac
+  source venvs/venv_mac/bin/activate
+  python server.py $1
   deactivate
-}
+ else
+  # Linux
+  source venvs/venv_linux/bin/activate
+  python server.py $1
+  deactivate
+ fi
 
-trap 'termination'  {1,2,3,15}
-
-source venv/bin/activate
-python server.py $1
-deactivate
+fi
